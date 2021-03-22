c68bc65a2f7ba3c8f9cf577f8ff5d69ec7da599c
MadMakZa <immakza@gmail.com>
2021-03-23 00:50:53 +0300

    cool theme

--- app/src/main/java/com/examples/testprogressbar/MainActivity.kt
+++ app/src/main/java/com/examples/testprogressbar/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.examples.testprogressbar
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,24 +11,37 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var progressBar: ProgressBar
     lateinit var btnBoom: Button
+    var currentProgress = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         progressBar = findViewById(R.id.progressBar)
         btnBoom = findViewById(R.id.btn_dmg)
 
         progressBar.max = 500
-        var currentProgress = 0
+
 
 
         btnBoom.setOnClickListener{
-            currentProgress ++
+
+            currentProgress +=10
             ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
-                .setDuration(0)
+                .setDuration(100)
                 .start()
+
+            progressFinish()
+        }
+    }
+
+    private fun progressFinish(){
+        if (currentProgress == 500){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
